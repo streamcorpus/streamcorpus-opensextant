5fa4083c680b635cf44dfdf1298f61598c5bfb87
John R. Frank <jrf@diffeo.com>
2015-01-06 16:51:40 +0000

    fixed encoding issues in HTTP POST wrote real tests that verify actual tokens noted errors in OpenSextant tagging... need to verify that the SOLR gaz is working

--- streamcorpus_opensextant/tests/test_tagger.py
+++ streamcorpus_opensextant/tests/test_tagger.py
@@ -3,19 +3,74 @@
 
 from __future__ import absolute_import
 import logging
+import pytest
 import sys
 
+
 import requests
-from streamcorpus import make_stream_item, Chunk
+from streamcorpus import make_stream_item, Chunk, EntityType
 from streamcorpus_pipeline._tokenizer import nltk_tokenizer
+from streamcorpus_pipeline._clean_html import clean_html
+from streamcorpus_pipeline._clean_visible import clean_visible
 
 from streamcorpus_opensextant.tagger import OpenSextantTagger
 
-def test_opensextant_tagger():
+logger = logging.getLogger('streamcorpus_pipeline.' + __name__)
+
+texts = [
+    (u'\u2602 on the John Smith traveling in Liberia. It continues with a second sentence about Paris.',
+     [[(u'\u2602', None),
+      ('on', None),
+      ('the', EntityType.PER),
+      ('John', EntityType.PER),
+      ('Smith', EntityType.PER),
+      ('traveling', None),
+      ('in', None),
+      ('Liberia.', None),],[  ## sadly not EntityType.LOC
+      ('It', None),
+      ('continues', None),
+      ('with', None),
+      ('a', None),
+      ('second', None),
+      ('sentence', None),
+      ('about', None),
+      ('Paris.', None),],   ## sadly not EntityType.LOC
+  ]),
+    (u"""Paris, France is not Paris, Texas.
+    Fran\u00e7oise Smith has lived in the cities of Montreal and Qu\u00e9bec, Canada.
+    And everyone else lives in France.
+    """,
+     [[('Paris,', None),
+      ('France', None),
+      ('is', None),
+      ('not', None),
+      ('Paris,', None),
+      ('Texas.', None),],[
+      (u'Fran\u00e7oise', EntityType.PER),
+      ('Smith', EntityType.PER),
+      ('has', None),
+      ('lived', None),
+      ('in', None),
+          ('the', EntityType.LOC),     ## wrong
+          ('cities', EntityType.LOC),  ## wrong
+      ('of', None),
+      ('Montreal', None),
+      ('and', None),
+      (u'Qu\u00e9bec,', None),
+      ('Canada.', None),],[
+      ('And', None),
+      ('everyone', None),
+      ('else', None),
+      ('lives', None),
+      ('in', None),
+      ('France.', None),]
+  ]),
+]  
+
+@pytest.mark.parametrize('text,tokens', texts)
+def test_opensextant_tagger(text, tokens):
 
     si = make_stream_item(10, 'fake_url')
-    text = u'\u2602 on the John Smith traveling in Liberia.' + \
-           u' It continues with a second sentence about Paris.'
     si.body.clean_visible = text.encode('utf8')
 
     tokenizer = nltk_tokenizer({})
@@ -24,11 +79,20 @@ def test_opensextant_tagger():
     tokenizer.process_item(si)
     ost.process_item(si)
 
+    for sent_idx, sent in enumerate(si.body.sentences['opensextant']):
+        logger.info([tok.token for tok in sent.tokens])
+        for idx, tok in enumerate(sent.tokens):
+            logger.info('sent_idx = %d, token idx = %d', sent_idx, idx)
+            assert tok.token.decode('utf8') == tokens[sent_idx][idx][0]
+            assert tok.entity_type == tokens[sent_idx][idx][1]
+
     
 def main():
-    logging.basicConfig()
+    logging.basicConfig(level=logging.DEBUG)
 
     session = requests.Session()
+    chtml = clean_html({})
+    cvisible = clean_visible({})
     tokenizer = nltk_tokenizer({})
     ost = OpenSextantTagger(OpenSextantTagger.default_config)
 
@@ -47,8 +111,16 @@ def main():
         path = '/tmp/test-chunk.sc.xz.gpg'
         open(path, 'wb').write(resp.content)
         for si in Chunk(path):
+            if '<div' in si.body.raw \
+               or '<a href' in si.body.raw \
+               or '<span' in si.body.raw:
+                si.body.media_type = 'text/html'
+            chtml(si, {})
+            cvisible(si, {})
             tokenizer.process_item(si)
             ost.process_item(si)
+
+
 
 
 if __name__ == '__main__':
